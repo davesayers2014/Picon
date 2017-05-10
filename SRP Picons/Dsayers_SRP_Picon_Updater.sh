8738fcc08bf3b608fca4acb9f1eeb02c3d8fa687
davesayers2014 <davesayers2012@gmail.com>
2017-05-10 07:08:14 +0100

    Create Dsayers_SRP_Picon_Updater.sh

---
+++ SRP Picons/Dsayers_SRP_Picon_Updater.sh
@@ -0,0 +1,24 @@
+#Remove Existing Picons
+echo 'Cleaning up existing Picons - Please Wait'
+sleep 4
+opkg remove enigma2-plugin-picons-snp-full.100x60-86x46.light.on.transparent --force-remove >/dev/null 2>&1
+opkg remove enigma2-plugin-picons-snp-full.220x132-190x102.light.on.transparent --force-remove >/dev/null 2>&1
+rm -rv /media/hdd/SkyQ >/dev/null 2>&1
+rm -rv /media/hdd/picons >/dev/null 2>&1
+rm -rv /media/hdd/picon >/dev/null 2>&1
+rm -rv /picon >/dev/null 2>&1
+mkdir -p /media/hdd/SkyQ
+
+#Download and unpack Channel SNP Picons
+echo 'Standard SRP Picons downloading - Please Wait'
+opkg install https://github.com/davesayers2014/Picon/blob/master/enigma2-plugin-picons-snp-full.100x60-86x46.light.on.transparent_2017-05-05--23-44-42_all.ipk?raw=true --force-depends >/dev/null 2>&1
+echo 'Standard SRP Picons being moved into correct position - Please Wait'
+mv -f /picon /hdd/
+echo 'SkyQ SNP Picons downloading - Please Wait'
+opkg install https://github.com/davesayers2014/Picon/blob/master/enigma2-plugin-picons-snp-full.220x132-190x102.light.on.transparent_2017-05-05--23-44-42_all.ipk?raw=true --force-depends >/dev/null 2>&1
+echo 'SkyQ SRP Picons being moved into correct position - Please Wait'
+mv -f /picon /hdd/SkyQ
+echo 'OK - All done'
+sleep 5
+exit 1
+fi
