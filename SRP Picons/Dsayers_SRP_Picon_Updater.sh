e8e796a6d88bcd21d64128d4895918cb65ebe6be
davesayers2014 <davesayers2012@gmail.com>
2017-05-10 19:03:53 +0100

    Update Dsayers_SRP_Picon_Updater.sh

--- SRP Picons/Dsayers_SRP_Picon_Updater.sh
+++ SRP Picons/Dsayers_SRP_Picon_Updater.sh
@@ -15,7 +15,7 @@ opkg install https://github.com/davesayers2014/Picon/blob/master/SRP%20Picons/en
 echo 'Standard SRP Picons being moved into correct position - Please Wait'
 mv -f /picon /hdd/
 echo 'SkyQ SRP Picons downloading - Please Wait'
-https://github.com/davesayers2014/Picon/blob/master/SRP%20Picons/enigma2-plugin-picons-snp-full.220x132-190x102.light.on.transparent_2017-05-09--01-51-03_all.ipk?raw=true --force-depends >/dev/null 2>&1
+opkg install https://github.com/davesayers2014/Picon/blob/master/SRP%20Picons/enigma2-plugin-picons-srp-full.220x132-190x102.light.on.transparent_2017-05-09--01-51-03_all.ipk?raw=true --force-depends >/dev/null 2>&1
 echo 'SkyQ SRP Picons being moved into correct position - Please Wait'
 mv -f /picon /hdd/SkyQ
 echo 'OK - All done'
