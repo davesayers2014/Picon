c0a35abf89e37e23b1820de04bf510a57d7375ad
davesayers2014 <davesayers2012@gmail.com>
2017-05-10 07:52:08 +0100

    Update Dsayers_SNP_Picon_Updater.sh

--- SNP_Picons/Dsayers_SNP_Picon_Updater.sh
+++ SNP_Picons/Dsayers_SNP_Picon_Updater.sh
@@ -11,11 +11,11 @@ mkdir -p /media/hdd/SkyQ
 
 #Download and unpack Channel SNP Picons
 echo 'Standard SNP Picons downloading - Please Wait'
-opkg install https://github.com/davesayers2014/Picon/blob/master/enigma2-plugin-picons-snp-full.100x60-86x46.light.on.transparent_2017-05-05--23-44-42_all.ipk?raw=true --force-depends >/dev/null 2>&1
+opkg install https://github.com/davesayers2014/Picon/blob/master/SNP_Picons/enigma2-plugin-picons-snp-full.100x60-86x46.light.on.transparent_2017-05-09--01-51-03_all.ipk?raw=true --force-depends >/dev/null 2>&1
 echo 'Standard SNP Picons being moved into correct position - Please Wait'
 mv -f /picon /hdd/
 echo 'SkyQ SNP Picons downloading - Please Wait'
-opkg install https://github.com/davesayers2014/Picon/blob/master/enigma2-plugin-picons-snp-full.220x132-190x102.light.on.transparent_2017-05-05--23-44-42_all.ipk?raw=true --force-depends >/dev/null 2>&1
+opkg install https://github.com/davesayers2014/Picon/blob/master/SNP_Picons/enigma2-plugin-picons-snp-full.220x132-190x102.light.on.transparent_2017-05-09--01-51-03_all.ipk?raw=true --force-depends >/dev/null 2>&1
 echo 'SkyQ SNP Picons being moved into correct position - Please Wait'
 mv -f /picon /hdd/SkyQ
 echo 'OK - All done'
