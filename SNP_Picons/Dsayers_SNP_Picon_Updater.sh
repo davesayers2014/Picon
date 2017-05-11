63762bd80cb6522fe37681849c3d240e560ba40c
davesayers2014 <davesayers2012@gmail.com>
2017-05-11 07:25:58 +0100

    Cosmetic changes

--- SNP_Picons/Dsayers_SNP_Picon_Updater.sh
+++ SNP_Picons/Dsayers_SNP_Picon_Updater.sh
@@ -1,6 +1,22 @@
-#Remove Existing Picons
-echo 'Cleaning up existing Picons - Please Wait'
+#!/bin/sh
+echo '###########################'
+echo '# DAYERS SNP PICON UPDATER#'
+echo '###########################'
+echo $LINE
+echo 'OUR LATEST PICONS JUST GOT BETTER'
+echo 'DSAYERS PICONS VERSION 1.2'
+echo $LINE
 sleep 4
+echo 'THIS PLUGIN WILL ATTEMPT TO DOWNLOAD PICONS TO HDD AND HDD/SKYQ'
+echo 'LOCATIONS AND THEN UPDATE'
+echo 'YOUR PICONS TO THE LATEST PICONS'
+echo $LINE
+echo 'LETS BEGIN THE PROCESS'
+
+#Remove Existing Picons
+echo $LINE
+echo $LINE
+echo 'CLEANING UP EXISTING PICONS - PLEASE WAIT'
 opkg remove enigma2-plugin-picons-snp-full.100x60-86x46.light.on.transparent --force-remove >/dev/null 2>&1
 opkg remove enigma2-plugin-picons-snp-full.220x132-190x102.light.on.transparent --force-remove >/dev/null 2>&1
 rm -rv /media/hdd/SkyQ >/dev/null 2>&1
@@ -8,17 +24,26 @@ rm -rv /media/hdd/picons >/dev/null 2>&1
 rm -rv /media/hdd/picon >/dev/null 2>&1
 rm -rv /picon >/dev/null 2>&1
 mkdir -p /media/hdd/SkyQ
+sleep 2
+echo $LINE
 
-#Download and unpack Channel SNP Picons
-echo 'Standard SNP Picons downloading - Please Wait'
+#Download and unpack Standard SNP Picons
+echo 'STANDARD SNP PICONS DOWNLOADING - PLEASE WAIT'
+echo $LINE
 opkg install https://github.com/davesayers2014/Picon/blob/master/SNP_Picons/enigma2-plugin-picons-snp-full.100x60-86x46.light.on.transparent_2017-05-09--01-51-03_all.ipk?raw=true --force-depends >/dev/null 2>&1
-echo 'Standard SNP Picons being moved into correct position - Please Wait'
+echo 'STANDARD SNP PICONS BEING MOVED INTO CORRECT POSITION - PLEASE WAIT'
 mv -f /picon /hdd/
-echo 'SkyQ SNP Picons downloading - Please Wait'
+echo $LINE
+echo $LINE
+
+#Download and unpack SkyQ SNP Picons
+echo 'SkyQ SNP PICONS DOWNLOADING - PLEASE WAIT'
+echo $LINE
 opkg install https://github.com/davesayers2014/Picon/blob/master/SNP_Picons/enigma2-plugin-picons-snp-full.220x132-190x102.light.on.transparent_2017-05-09--01-51-03_all.ipk?raw=true --force-depends >/dev/null 2>&1
-echo 'SkyQ SNP Picons being moved into correct position - Please Wait'
+echo 'SkyQ SNP PICONS BEING MOVED INTO CORRECT POSITION - PLEASE WAIT'
 mv -f /picon /hdd/SkyQ
-echo 'OK - All done'
-sleep 5
-exit 0
+sleep 4
+echo $LINE
+echo 'OK - All DONE PRESS EXIT'
+exit 1
 fi
