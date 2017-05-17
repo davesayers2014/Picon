a913d5515c4182a3adaa01b16ef9ce773c6eca84
davesayers2014 <davesayers2012@gmail.com>
2017-05-17 08:23:43 +0100

    Add SRP Picons 2017-05-17

--- SRP Picons/Dsayers_SRP_Picon_Updater.sh
+++ SRP Picons/Dsayers_SRP_Picon_Updater.sh
@@ -30,7 +30,7 @@ echo $LINE
 #Download and unpack Channel SRP Picons
 echo 'STANDARD SRP PICONS DOWNLOADING - PLEASE WAIT'
 echo $LINE
-opkg install https://github.com/davesayers2014/Picon/blob/master/SRP%20Picons/enigma2-plugin-picons-srp-full.100x60-86x46.light.on.transparent_2017-05-14--14-47-01_all.ipk?raw=true --force-depends >/dev/null 2>&1
+opkg install https://github.com/davesayers2014/Picon/blob/master/SRP%20Picons/enigma2-plugin-picons-srp-full.100x60-86x46.light.on.transparent_2017-05-15--12-02-34_all.ipk?raw=true --force-depends >/dev/null 2>&1
 echo 'STANDARD SRP PICONS BEING MOVED INTO CORRECT POSITION - PLEASE WAIT'
 mv -f /picon /hdd/
 echo $LINE
