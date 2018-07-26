aa8b3346af0c89a90775bc477c97d2a97ad867e8
davesayers2014 <davesayers2012@gmail.com>
2018-07-26 16:41:40 +0100

    Picons 2018-07-22

--- SNP_Picons/Dsayers_SNP_Picon_Updater.sh
+++ SNP_Picons/Dsayers_SNP_Picon_Updater.sh
@@ -30,14 +30,14 @@ echo $LINE
 #Download and unpack Standard SNP Picons
 echo 'STANDARD SNP PICONS DOWNLOADING - PLEASE WAIT'
 echo $LINE
-opkg install https://github.com/davesayers2014/Picon/blob/master/SNP_Picons/enigma2-plugin-picons-snp-full.100x60-86x46.light.on.transparent_2018-06-05--23-06-37_all.ipk?raw=true --add-dest hdd:/media/hdd -d hdd --force-depends >/dev/null 2>&1
+opkg install https://github.com/davesayers2014/Picon/blob/master/SNP_Picons/enigma2-plugin-picons-snp-full.100x60-86x46.light.on.transparent_2018-07-22--14-40-30_all.ipk?raw=true --add-dest hdd:/media/hdd -d hdd --force-depends >/dev/null 2>&1
 echo $LINE
 echo $LINE
 
 #Download and unpack SkyQ SNP Picons
 echo 'SkyQ SNP PICONS DOWNLOADING - PLEASE WAIT'
 echo $LINE
-opkg install https://github.com/davesayers2014/Picon/blob/master/SNP_Picons/enigma2-plugin-picons-snp-full.220x132-220x132.light.on.transparent_2018-06-05--23-06-37_all.ipk?raw=true --add-dest hdd:/media/hdd/SkyQ -d hdd --force-depends >/dev/null 2>&1
+opkg install https://github.com/davesayers2014/Picon/blob/master/SNP_Picons/enigma2-plugin-picons-snp-full.220x132-220x132.light.on.transparent_2018-07-22--14-40-30_all.ipk?raw=true --add-dest hdd:/media/hdd/SkyQ -d hdd --force-depends >/dev/null 2>&1
 rm -rv /media/hdd/var >/dev/null 2>&1
 sleep 4
 echo $LINE
