d0b7199477eb49d32e45ec3959cfbb84ce77af5e
davesayers2014 <davesayers2012@gmail.com>
2021-02-07 11:31:12 +0000

    Update Dsayers_SNP_Picon_Updater.sh

--- SNP_Picons/Dsayers_SNP_Picon_Updater.sh
+++ SNP_Picons/Dsayers_SNP_Picon_Updater.sh
@@ -9,6 +9,39 @@ echo $LINE
 sleep 4
 echo 'THIS PLUGIN WILL ATTEMPT TO DOWNLOAD PICONS TO HDD AND HDD/SKYQ'
 echo 'LOCATIONS AND THEN UPDATE'
-echo 'believe who I am now change the contract'
+echo 'YOUR PICONS TO THE LATEST PICONS'
+echo $LINE
+echo 'LETS BEGIN THE PROCESS'
+
+#Remove Existing Picons
+echo $LINE
+echo $LINE
+echo 'CLEANING UP EXISTING PICONS - PLEASE WAIT'
+opkg remove enigma2-plugin-picons-snp-full.100x60-86x46.light.on.transparent --force-remove >/dev/null 2>&1
+opkg remove enigma2-plugin-picons-snp-full.220x132-220x132.light.on.transparent --force-remove >/dev/null 2>&1
+opkg remove enigma2-plugin-picons-snp-full.220x132-190x102.light.on.transparent --force-remove >/dev/null 2>&1
+rm -rv /media/hdd/SkyQ >/dev/null 2>&1
+rm -rv /media/hdd/picons >/dev/null 2>&1
+rm -rv /media/hdd/picon >/dev/null 2>&1
+rm -rv /picon >/dev/null 2>&1
+mkdir -p /media/hdd/SkyQ
+sleep 2
+echo $LINE
+
+#Download and unpack Standard SNP Picons
+echo 'STANDARD SNP PICONS DOWNLOADING - PLEASE WAIT'
+echo $LINE
+opkg install "https://github.com/picons/picons/releases/download/2020-11-05--00-44-00/enigma2-plugin-picons-snp-full.100x60-86x46.light.on.transparent_2020-11-05--00-44-00_all.ipk" --add-dest hdd:/media/hdd -d hdd --force-depends >/dev/null 2>&1
+echo $LINE
+echo $LINE
+
+#Download and unpack SkyQ SNP Picons
+echo 'SkyQ SNP PICONS DOWNLOADING - PLEASE WAIT'
+echo $LINE
+opkg install "https://github.com/picons/picons/releases/download/2020-11-05--00-44-00/enigma2-plugin-picons-snp-full.220x132-190x102.light.on.transparent_2020-11-05--00-44-00_all.ipk" --add-dest hdd:/media/hdd/SkyQ -d hdd --force-depends >/dev/null 2>&1
+rm -rv /media/hdd/var >/dev/null 2>&1
+sleep 4
+echo $LINE
+echo 'OK - All DONE PRESS EXIT'
 exit 1
 fi
